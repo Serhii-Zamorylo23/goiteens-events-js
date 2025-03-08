87a4c77a77551d69a32cb3ab54c8aebf551a06d7
Serhii-Zamorylo23 <129538617+Serhii-Zamorylo23@users.noreply.github.com>
2025-03-08 18:47:29 +0200

    test

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PageBoard from './components/PageBoard/PageBoard';
-import eventsjson from './components/upcoming-events.json'
-export const App = () => {
-  return (
-    <PageBoard events={eventsjson}/>
-  );
-};
