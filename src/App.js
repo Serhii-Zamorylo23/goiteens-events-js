2e1841143dcadfae83b21df90aa964ee6e05de09
Serhii-Zamorylo23 <129538617+Serhii-Zamorylo23@users.noreply.github.com>
2025-03-09 22:18:45 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import React from 'react';
 import './App.css';
-
+import PageBoard from './Components/PageBoard/PageBoard';
+import eventsjson from './Components/upcoming-events.json'
 function App() {
   return (
-    <></>
+    <PageBoard events={eventsjson}/>
   );
 }
 
